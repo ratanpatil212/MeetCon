1a83b4a99dc29242d34dd786a3b7c7858a201e5f
ratanpatil212 <ratanpatil212@gmail.com>
2021-12-18 10:37:44 +0530

    Co-authored-by: Atharva Patilpate <atharva.patilpate@gmail.com>

---
+++ DBSchemas/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fname: {
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+
+    bestFriend:{
+    type: mongoose.SchemaTypes.ObjectId,
+    ref:"User",
+},
+    hobbies:[String],
+    address: {
+        street: String,
+        city: String,
+    }
+})
+
+module.exports = mongoose.model("User", userSchema)
--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
+const User = require('./DBSchemas/User.js')
 const BodyParser = require('body-parser');
+const mongojs = require('mongojs');
+
 app.use(BodyParser());
+const port = 3000;
+const DB = "mongodb+srv://adminritzdb:adminritzmongo@cluster0.j61gc.mongodb.net/ritzdb?retryWrites=true&w=majority"
+mongoose.connect(DB,()=>{
+    console.log("Connected to MongoDB-Atlas ");
+})
 
 app.use(express.static('public'))
 app.use(express.static('fonts'))
@@ -11,6 +19,8 @@ app.use(express.static('vendor'))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/html/main.html");
 });
+
+global.db = mongojs(DB);
 
 // for templating: pug
 app.set('view engine', 'pug');
@@ -39,9 +49,59 @@ app.get("/:id", (req, res) => {
     res.sendFile(__dirname + "/html/"+req.params.id+".html");
 });
 
+//receiving the psot request from signup
+var ffname;
+var llname;
+var mmail;
+var ppassword;
 
+app.post('/',(req, res)=>{
+    ffname = req.body.firstname;
+    llname = req.body.lastname;
+    mmail = req.body.email;
+    ppassword = req.body.password;
+    run();
+    res.sendFile(__dirname +"/html/main.html")
+})
+
+app.post('/',(req, res)=>{
+    mmail = req.body.email;
+    ppassword = req.body.password;
+    
+
+})
 
 
 app.listen(port,(req, res) => {
     console.log("listening on port 3000");
 });
+
+// Mongodb Insert Data
+async function run(){
+    try {
+        if(db.users.find({ "email" : mmail})==null) {
+            console.log("Appears to be a new email");
+            const user = await User.create({
+                fname : ffname,
+                lname : llname,
+                email : mmail,
+                password : ppassword
+            });
+            console.log("Account Created");
+        }
+        else{
+            console.log("User already exists");
+            console.log("Couldnt create Account");
+        }
+        
+
+    } catch(err) {
+        console.log(err);
+    }
+    
+
+    
+}
+
+
+// mongodb+srv://adminritzdb:<password>@cluster0.j61gc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
